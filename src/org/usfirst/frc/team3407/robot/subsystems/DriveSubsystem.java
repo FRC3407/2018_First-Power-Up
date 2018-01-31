c0b3fe99b469b9d0790d3749e432c532b3628dc8
Mustybots <moundsviewrobotics@gmail.com>
2018-01-30 20:25:05 -0600

    initial commit w/ basic drivetrain

---
+++ src/org/usfirst/frc/team3407/robot/subsystems/DriveSubsystem.java
@@ -0,0 +1,36 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc.team3407.robot.subsystems;
+
+import org.usfirst.frc.team3407.robot.RobotMap;
+import org.usfirst.frc.team3407.robot.commands.DriveCommand;
+
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+
+public class DriveSubsystem extends Subsystem {
+	private Victor 	m_FrontLeft = new Victor(RobotMap.FRONT_LEFT_MOTOR);
+	Victor m_BackLeft = new Victor(RobotMap.BACK_LEFT_MOTOR) ;
+	SpeedControllerGroup m_left = new SpeedControllerGroup(m_FrontLeft, m_BackLeft);
+	
+	Victor m_FrontRight = new Victor(RobotMap.FRONT_RIGHT_MOTOR);
+	Victor m_BackRight = new Victor(RobotMap.BACK_RIGHT_MOTOR);
+	SpeedControllerGroup m_right = new SpeedControllerGroup(m_FrontRight, m_BackRight);
+	
+	DifferentialDrive m_drive = new DifferentialDrive(m_left, m_right);
+	
+	public void tank(double leftPow, double rightPow) {
+		m_drive.tankDrive(leftPow, rightPow);
+	}
+	public void initDefaultCommand() {
+	setDefaultCommand(new DriveCommand());
+	}
+}
